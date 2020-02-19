7e9d3ee423fdd6157b317219aba92f5923733d04
Sebastiaan Dekker <sebastiaan@Sebastiaans-MBP.codaisseur.local>
2020-02-19 17:14:41 +0100

    Homepage components and reducer to show products

---
+++ src/components/homepage/ProductExcerpt.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export default class ProductExcerpt extends Component {
+  render() {
+    return (
+      <div className="col-4 mb-4">
+        <div className="card">
+          <img className="card-img-top" src={this.props.imageUrl} alt="Card" />
+          <div className="card-body">
+            <h3 className="card-title">{this.props.name}</h3>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            {this.props.inStock ? (
+              <p className="text-success font-weight-bold">instock!</p>
+            ) : (
+              <p className="text-danger font-weight-bold">not in stock</p>
+            )}
+            <Link to={`/product/${this.props.id}`} className="btn btn-primary">
+              Go to Product
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
