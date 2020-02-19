f3e65c77f0a36b16e5ac721af84ed50c03f95491
Sebastiaan Dekker <sebastiaan@Sebastiaans-MBP.codaisseur.local>
2020-02-19 15:31:15 +0100

    Bootstrap <link> and Header component in App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./App.css";
 import { Switch, Route } from "react-router";
 import Homepage from "./components/homepage/Homepage";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route path="/" component={Homepage} />
       </Switch>
